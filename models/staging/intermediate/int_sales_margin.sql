21897dea3d4a043220c0364066b6b67dfeeaef7b
Victoire <victoirechapuis@gmail.com>
2024-07-24 09:00:29 +0000

    change left join

--- models/staging/intermediate/int_sales_margin.sql
+++ models/staging/intermediate/int_sales_margin.sql
@@ -8,4 +8,4 @@ raw_gz_product.purchase_price,
 (raw_gz_sales.quantity*purchase_price) as purchase_cost
 raw_gz_sales.revenue-(raw_gz_sales.quantity*purchase_price) as margin
 from {{ ref('stg_gz_raw_data__raw_gz_sales') }} 
-join {{ ref('stg_gz_raw_data__raw_gz_product') }} on raw_gz_product.products_id = raw_gz_sales.pdt_id
+left join {{ ref('stg_gz_raw_data__raw_gz_product') }} using  (pdt_id)
