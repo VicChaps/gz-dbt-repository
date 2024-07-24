89baa653787c55a561d8ce8e4b9ed50f614c416b
Victoire <victoirechapuis@gmail.com>
2024-07-24 13:03:27 +0000

    test

--- models/staging/intermediate/int_orders_operational.sql
+++ models/staging/intermediate/int_orders_operational.sql
@@ -1,5 +1,4 @@
-SELECT
-  o.orders_id
+o.orders_id
   ,o.date_date
   ,ROUND(o.margin + s.shipping_fee - (s.logcost + s.ship_cost),2) AS operational_margin 
   ,o.quantity
@@ -10,6 +9,6 @@ SELECT
   ,s.logcost
   ,s.ship_cost
 FROM {{ref("int_orders_margin")}} o
-LEFT JOIN {{ref("stg_raw_ship")}} s 
+LEFT JOIN {{ref("stg_gz_raw_data__raw_gz_ship")}} s 
   USING(orders_id)
 ORDER BY orders_id desc
