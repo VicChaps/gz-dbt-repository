0421a3695232736c3e4404efb8e7bb896fb2eb1f
Victoire <victoirechapuis@gmail.com>
2024-07-24 13:40:33 +0000

    t

--- models/staging/intermediate/int_orders_margin.sql
+++ models/staging/intermediate/int_orders_margin.sql
@@ -3,8 +3,8 @@ select
   date_date,
   round(SUM(revenue),2) as revenue,
   round(SUM(quantity),2) as quantity,
-  round(SUM(purchse_cost),2) as purchase_cost,
+  round(SUM(purchase_cost),2) as purchase_cost,
   round(SUM(margin),2) as margin
-from {{ ref('stg_gz_raw_data__raw_gz_sales') }}
+from {{ ref("stg_gz_raw_data__raw_gz_sales") }}
 group by orders_id,date_date
 order by orders_id desc
